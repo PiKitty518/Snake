9d3c4bc20d938d1bebd696bafc6a2bc659b406cb
Kevin Lynch <henrymccauleylynch@gmail.com>
2020-08-18 20:42:31 -0400

    Part 2

--- Snake.py
+++ Snake.py
@@ -1,4 +1,7 @@
 import turtle
+import time
+
+delay = 0.1
 
 wn=turtle.Screen()
 wn.title("Snake Game by PiKitty518")
@@ -6,16 +9,50 @@ wn.bgcolor("green")
 wn.setup(width=600,height=600)
 wn.tracer(0)
 
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
 
+def go_up():
+    head.direction = "up"
+def go_down():
+    head.direction = "down"
+def go_right():
+    head.direction = "right"
+def go_left():
+    head.direction = "left"
 
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y+20)
 
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y-20)
 
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x-20)
 
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x+20)
 
+wn.listen()
+wn.onkeypress(go_up,"w")
+wn.onkeypress(go_down,"s")
+wn.onkeypress(go_left,"a")
+wn.onkeypress(go_right,"d")
 
-
-
-
+while True:
+    wn.update()
+    move()
+    time.sleep(delay)
 
 
 wn.mainloop()
