567fca3b3e1912b02f7b4ebb4901ce444549d7af
Kevin Lynch <henrymccauleylynch@gmail.com>
2020-08-18 20:11:36 -0400

    Part 1
    Screen

---
+++ Snake.py
@@ -0,0 +1,21 @@
+import turtle
+
+wn=turtle.Screen()
+wn.title("Snake Game by PiKitty518")
+wn.bgcolor("green")
+wn.setup(width=600,height=600)
+wn.tracer(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+wn.mainloop()
