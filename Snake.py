97d954d95afe55098cc037b858719c6fcf01fe48
Kevin Lynch <henrymccauleylynch@gmail.com>
2020-08-19 21:25:14 -0400

    Part 5
    border collistion

--- Snake.py
+++ Snake.py
@@ -74,6 +74,16 @@ while True:
         new_segment.penup()
         segments.append(new_segment)
 
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction="stop"
+
+        for segment in segments:
+            segment.goto(1000,1000)
+
+        segments.clear()
+
     for index in range(len(segments)-1,0,-1):
         x = segments[index-1].xcor()
         y = segments[index-1].ycor()
